1c51364b2e45561951e49f23ac252f30234e2ab0
Daniela Baron <baron.daniela@gmail.com>
2021-02-27 17:05:35 -0500

    Initial commit

---
+++ data_science.rb
@@ -0,0 +1,36 @@
+# Unlike Python, Ruby doesn't have native tuples.
+Tuple = Struct.new(:_1, :_2)
+
+module DataScience
+  # RLE (run-length encoding): Encode each character by the number of times it appears consecutively.
+  def self.rle(input)
+    # Keep track of letters that have been seen for incrementing consecutive counter.
+    track = {}
+
+    # Array of results to be returned.
+    result = []
+
+    # Initialize based on first character in input string
+    previous_letter = input[0]
+    track[previous_letter] = Tuple.new(previous_letter, 1)
+    result << track[previous_letter]
+
+    # Process input string one characetr at a time, starting from second character
+    for i in 1..input.length - 1 do
+      current_letter = input[i]
+
+      # If the current letter was just seen in the previous iteration, increment its tracking counter
+      track[previous_letter]._2 += 1 if current_letter == previous_letter
+
+      # Otherwise start tracking a new letter and add it to results
+      if current_letter != previous_letter
+        track[current_letter] = Tuple.new(current_letter, 1)
+        result << track[current_letter]
+      end
+
+      # The letter that has just been processed (current) now becomes the previous for next iteration
+      previous_letter = current_letter
+    end
+    result
+  end
+end
---
+++ spec/data_science_spec.rb
@@ -0,0 +1,27 @@
+require './data_science'
+
+describe DataScience do
+  describe 'Run Length Encoding' do
+    it 'Counts consecutive occurrences of letters in a string' do
+      result = DataScience.rle('aaaabbbcca')
+
+      a4 = Tuple.new('a', 4)
+      b3 = Tuple.new('b', 3)
+      c2 = Tuple.new('c', 2)
+      a1 = Tuple.new('a', 1)
+
+      expect(result).to eq([a4, b3, c2, a1])
+      expect(result.length).to eq(4)
+    end
+
+    it 'Handles alternating letters' do
+      result = DataScience.rle('abab')
+
+      a1 = Tuple.new('a', 1)
+      b1 = Tuple.new('b', 1)
+
+      expect(result).to eq([a1, b1, a1, b1])
+      expect(result.length).to eq(4)
+    end
+  end
+end
